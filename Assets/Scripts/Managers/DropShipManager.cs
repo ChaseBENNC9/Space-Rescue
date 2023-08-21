b8a344e1f58316479cca8e2b238dcc6bba2bb9ce
ChaseBENNC9 <104808214+ChaseBENNC9@users.noreply.github.com>
2023-08-21 22:03:40 +1200

    wil this commit fail

--- Assets/Scripts/Managers/DropShipManager.cs
+++ Assets/Scripts/Managers/DropShipManager.cs
@@ -8,7 +8,7 @@ using UnityEngine;
 
 public class DropShipManager : MonoBehaviour
 {
-    private const int numbers = 554;
+    private const int numbers = 5554;
     public void OnTriggerStay(Collider other)
     {
         if (other.gameObject.tag == "Player")
