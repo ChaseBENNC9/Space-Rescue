e85c9b56e01f1413008107a81e3309a4d87bcf14
ChaseBENNC9 <104808214+ChaseBENNC9@users.noreply.github.com>
2023-08-21 22:10:17 +1200

    seriously

--- Assets/Scripts/Managers/DropShipManager.cs
+++ Assets/Scripts/Managers/DropShipManager.cs
@@ -8,7 +8,7 @@ using UnityEngine;
 
 public class DropShipManager : MonoBehaviour
 {
-    private const int numbers = 545454;
+    private const int numbers = 4;
     public void OnTriggerStay(Collider other)
     {
         if (other.gameObject.tag == "Player")
