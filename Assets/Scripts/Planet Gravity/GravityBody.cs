fa7d261549b0e5416cf415c10ec9c7ce2f25c877
ChaseBENNC9 <104808214+ChaseBENNC9@users.noreply.github.com>
2023-08-27 20:44:36 +1200

    Update GravityBody.cs

--- Assets/Scripts/Planet Gravity/GravityBody.cs
+++ Assets/Scripts/Planet Gravity/GravityBody.cs
@@ -1,10 +1,10 @@
 using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+using System.Collections.Generic; 
+using UnityEngine; 
 
-public class GravityBody : MonoBehaviour
+public class GravityBody : MonoBehaviour          
 {
-    public GravityAttractor attractor;
+    public GravityAttractor attractor;            
     private Transform myTransform;
     private Rigidbody rb;
     private GameObject planet;
